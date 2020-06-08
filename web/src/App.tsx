fa2e909fc7ab844b39ef5878d934a1467a8e932c
Carolina Almeida <carolina.almeida@fabricadeaplicativos.com.br>
2020-06-07 22:36:51 -0300

    full application

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
